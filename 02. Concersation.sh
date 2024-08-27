905f588670650f977aa5123d92b6dca1766db23e
borugaddarajesh <rajeshborugadda@gmail.com>
2024-08-27 19:34:08 +0530

    added converstation script

---
+++ 02. Concersation.sh
@@ -0,0 +1,7 @@
+02. Concersation.sh
+#!/bin/bash
+
+echo "Rajesh: : Hi suresh, How are you?"
+echo "Suresh: : Hello Rajesh, I am fine. How are you?"
+echo "Rajesh: : I am doing good suresh, whats going on?"
+echo "Suresh: : I started learing Shell scripting Rajesh"
