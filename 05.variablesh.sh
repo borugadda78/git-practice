dbc86207a164abe5c77d78a7187734f28a1a0ecd
borugaddarajesh <rajeshborugadda@gmail.com>
2025-02-03 23:29:53 +0530

    variables

--- 05.variablesh.sh
+++ 05.variablesh.sh
@@ -5,3 +5,9 @@ echo "please enter your username ::"
 read USERNAME
 
 echo "username enter is: $USERNAME"
+
+echo "please enter your password ::"
+
+read PASSWORD
+
+echo "Password enter is: $PASSWORD"
